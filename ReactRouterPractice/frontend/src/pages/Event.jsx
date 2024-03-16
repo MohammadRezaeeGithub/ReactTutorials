6f60371d32154f8082b22ef2be378cdd3864e60d
Mohammad Rezaee <mohammadrezaee22@gmail.com>
2024-03-16 18:37:20 +0100

    completed

--- ReactRouterPractice/frontend/src/pages/Event.jsx
+++ ReactRouterPractice/frontend/src/pages/Event.jsx
@@ -18,14 +18,17 @@ function EventsPage() {
 export default EventsPage;
 
 export async function loader() {
+  //in the loader function we can not use any kind of react hooks
+  //they are allowed to use only in the components
   const response = await fetch("http://localhost:8080/events");
   if (!response.ok) {
     //....
     //return { isError: true, message: "Could not fetch data!" };
     //throw { message: "Could not fetch the data!" };
     // throw new Response(JSON.stringify({ message: "Could not fetch events!" }), {
-    //   status: 500,
-    // });
+    //   status: 500,    in this case react will execute the nearest error components in the router
+    //for example if we could have an error component in the root, it will be execute whenever we have an error
+    // }); on the other hand, in the component we can use useRouteError hook to use the information of this error
     return json({ message: "Could not fetch events!" }, { status: 500 });
   } else {
     // const resData = await response.json();
